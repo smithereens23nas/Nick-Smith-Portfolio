2311020b5b163f952229d095e8bd3ce75b447ce5
smiethereens23nas <nicholas23smith@gmail.com>
2022-04-01 18:04:11 -0400

    added experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/CartoonMe(6).png";
+import ME from "../../assets/1585959536342 (4).jpg";
 import { FaAward } from "react-icons/fa";
 import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 const About = () => {
   return (
     <section id="about">
@@ -22,16 +23,18 @@ const About = () => {
               <small>1+ Years Working</small>
             </article>
             <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>1+ Years Working</small>
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
             </article>
             <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>1+ Years Working</small>
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
             </article>
           </div>
+          <p>This is my paragraph that I am suppose to type and enter here.</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='Projects'>Experience</section>
-  )
-}
+    <section id="projects">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>Python</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>JQuery</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+{/* End of frontend */}
 
-export default Experience
+        <div className="experience__backend"></div>
+        <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>MongoDB</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>NoSQL</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>Heroku</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill />
+              <h4>PSQL</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
