225bec502ffdb9e2dc2de362944cd6b3af0c7e0a
smiethereens23nas <nicholas23smith@gmail.com>
2022-04-21 14:39:11 -0400

    edited about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
             <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Clients</h5>
-              <small>1</small>
+              <small>2</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
